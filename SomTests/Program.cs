1e0f1fb304678896bb57b2dcca9ccca907338ec1
DenisPashkov <pashkovdenis@gmail.com>
2018-04-21 22:00:31 +0300

    Add project files.

---
+++ SomTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SomTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
